d0c47801b9804c1087dd393672760f337400aaaa
ElchinNasirov <nasirov.elchin@gmail.com>
2020-07-27 18:16:26 -0700

    Added some code

--- knexfile.js
+++ knexfile.js
@@ -26,4 +26,17 @@ module.exports = {
 
     seeds: { directory: './database/seeds' },
   },
+
+  production: {
+    client: 'sqlite3',
+    connection: { filename: './database/auth.db3' },
+    useNullAsDefault: true,
+
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'dbmigrations',
+    },
+
+    seeds: { directory: './database/seeds' },
+  },
 };
